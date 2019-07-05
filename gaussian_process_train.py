e65a86181355c2773075b93324f769ab960df926
me <toolongtext@gmail.com>
2019-07-05 22:34:05 +0200

    fixed wrong score

--- gaussian_process_train.py
+++ gaussian_process_train.py
@@ -28,13 +28,13 @@ num_one = np.count_nonzero(target)
 print("Number of arrows ", num_one)
 print("Number of non arrows ",len(target)-num_one)
 
-x_train, x_test, y_train, y_test = train_test_split(features,target,test_size=0.07)
+x_train, x_test, y_train, y_test = train_test_split(features,target,test_size=0.1)
 
 kernel = 1.0 * RBF(1.0)
 #gpc = GaussianProcessClassifier(kernel=kernel, random_state=0).fit(features, target)
 gpc = GaussianProcessClassifier(kernel=kernel, random_state=0).fit(x_train, y_train)
 
-score = gpc.score(features,target)
+score = gpc.score(x_train,y_train)
 print("Accuracy of training data: ",score)
 print('Accuracy on the testing subset:',format(gpc.score(x_test,y_test)))
 
