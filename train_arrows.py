8020ce26d4c77fc1992249beb5a0cffd52e34bdc
me <toolongtext@gmail.com>
2019-10-20 17:42:46 +0200

    Created new data set and trained new ai. Now only features and targets are loaded.

--- train_arrows.py
+++ train_arrows.py
@@ -21,37 +21,32 @@ from sklearn.model_selection import train_test_split
 ###########################################################
 # Load training data and concatenate them.
 ##########################################################
-features = np.load("sign_features.npy")
-target = np.load("sign_target.npy")
-
-bad = np.load("false_features.npy")
-good = np.load("right_features.npy")
-
-# Shorten bad data to the size of good data. Ignore mixed data from features.
-bad = bad[np.random.choice(len(bad),len(good),replace=False),:]
-features = np.concatenate((features,bad,good), axis=0)
-target = np.concatenate((target,np.zeros(len(bad)),np.ones(len(good))))
+features = np.load("arrow_features.npy")
+target = np.load("arrow_target.npy")
 
 #############################################################
 # Get number of arrows and non arrows for comparison.
 #############################################################
 num_one = np.count_nonzero(target)
 print("Number of arrows ", num_one)
-print("Number of non arrows ",len(target)-num_one)
+print("Number of non arrows ", len(target) - num_one)
 
 #############################################################
-# Split data for training and testing, start training and testing and save.
+# Split data for training and testing, start training.
 #############################################################
-x_train, x_test, y_train, y_test = train_test_split(features,target,test_size=0.1)
+x_train, x_test, y_train, y_test = train_test_split(features, target, test_size=0.2)
 
 kernel = 1.0 * RBF(1.0)
 gpc = GaussianProcessClassifier(kernel=kernel, random_state=0).fit(x_train, y_train)
 
-score = gpc.score(x_train,y_train)
-print("Accuracy of training data: ",score)
-print('Accuracy on the testing subset:',format(gpc.score(x_test,y_test)))
+##############################################################
+# Testing and saving.
+##############################################################
+score = gpc.score(x_train, y_train)
+print("Accuracy of training data: ", score)
+print('Accuracy on the testing subset:', format(gpc.score(x_test, y_test)))
 
-with open("sign_ai.pkl","wb") as f:
-    _pickle.dump(gpc,f)
+with open("arrow_ai.pkl", "wb") as f:
+    _pickle.dump(gpc, f)
 
 print("done")
