2f6f002ba5898268a6e5bfd49af58201be57a172
tgwaAo <toolongtext@gmail.com>
2019-10-25 17:05:06 +0200

    Codestyle and docstyle corrected.
    Former-commit-id: 0aa8cbd830579b21c25dbfbb7ef6dd6a413fe178

--- extract_features.py
+++ extract_features.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 """
-Published under GNU General Public License v3.0
+Published under GNU General Public License v3.0.
 
-You should have recieved a copy of the license GNU GPLv3. 
-If not, see 
+You should have recieved a copy of the license GNU GPLv3.
+If not, see
 http://www.gnu.org/licenses/
 
 Function to support extraction and sorting of features of arrows.
 
-@author: me
+@author: Uwe Simon
 """
 import cv2
 import numpy as np
@@ -18,53 +18,55 @@ import numpy as np
 NUM_FEATURES = 14
 NUM_POINTS = 5
 
+
 def find_contours(image):
-    """ 
+    """
     Change image with some filters and return contours in changed image.
-    
+
     The original image gets changed by morphological gradient, then
     dilated, eroded. After that canny and a additional closing are used.
     In the end findContours searches for contours.
-    
+
     Parameters
     ----------
     image : Mat
         Image used to find contours.
-        
+
     Returns
     -------
     list
         List of found Contours.
-    
+
     """
     kernel = np.ones((5, 5), np.uint8)
     image = cv2.morphologyEx(image, cv2.MORPH_GRADIENT, kernel)
     dilated = cv2.dilate(image, None, iterations=1)
-    filtered= cv2.erode(dilated, None, iterations=1)
+    filtered = cv2.erode(dilated, None, iterations=1)
     canny = cv2.Canny(filtered, 200, 400)
     dilated = cv2.dilate(canny, None, iterations=1)
-    filtered= cv2.erode(dilated, None, iterations=1)
-    
-    im2, con, hierarchy = cv2.findContours(filtered, 
-                                           cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
-    
+    filtered = cv2.erode(dilated, None, iterations=1)
+
+    im2, con, hierarchy = cv2.findContours(filtered,
+                                           cv2.RETR_TREE,
+                                           cv2.CHAIN_APPROX_SIMPLE)
     return con
+
 
 def get_shape_points(c):
     """
     Return main points and center data of contour c.
-    
+
     Parameters
     ----------
     c : int32 matrix
         Contour used to extract information about it.
-    
+
     Returns
     -------
     tuple
         Returns a tuple of a matrix containing the 'important' points and
         the x- and y-coordinates of the center of the contour.
-        
+
     """
     M = cv2.moments(c)
     if (M["m00"] != 0):
@@ -75,17 +77,17 @@ def get_shape_points(c):
         y_c = -1
     retval = cv2.arcLength(c, True)
     points = cv2.approxPolyDP(c, 0.04 * retval, True)
-    
     return (points, x_c, y_c)
-    
+
+
 def prepare_data(im, c, points, x_c, y_c, shape_ref):
     """
     Prepare data to have all information ready to check this contour.
-    
+
     A collection of nearly all functions above. This is a 'faster' way to
-    prepare data to check, whether this contour is the one an arrow would 
+    prepare data to check, whether this contour is the one an arrow would
     have or not. Shape is compared to a reference shape only here.
-    
+
     Parameters
     ----------
     im : Mat
@@ -100,14 +102,14 @@ def prepare_data(im, c, points, x_c, y_c, shape_ref):
         Y-coordinate of the center of this contour.
     shape_ref : int32
         Matrix of points describing a reference shape.
-        
+
     Returns
     -------
     Array
-        Array containing the prepared data to check being the searched 
-        symbol or not. It contains angles, ranges, a distance, saturation and 
+        Array containing the prepared data to check being the searched
+        symbol or not. It contains angles, ranges, a distance, saturation and
         value, a simlarity and percentage of an area.
-        Angles are measured from the middle of the closest points to each 
+        Angles are measured from the middle of the closest points to each
         point. Vertex is the middle of the shape.
         The ranges are in percent in respect to the original sum of them.
         The distance is calculated between the middle of the two closest points
@@ -116,23 +118,27 @@ def prepare_data(im, c, points, x_c, y_c, shape_ref):
         The similarity is calculated as distance of Hu Moments.
         Calculate the percentage of the contour compared to its rectangular
         area.
-        
+
     """
     ranges, angles, r_sum = get_ranges_and_angles(points, x_c, y_c)
     ranges = ranges_in_percentage(ranges, r_sum)
     dist, alpha, x_m, y_m = data_between_nearest(points, x_c, y_c)
     angles, ranges = sort_angles(angles, ranges, alpha)
     hsv = get_color_in_hsv(im, c)
-    similarity = np.float(cv2.matchShapes(c, shape_ref, cv2.CONTOURS_MATCH_I2, 0))
+    similarity = np.float(cv2.matchShapes(c, shape_ref, cv2.CONTOURS_MATCH_I2,
+                                          0))
     area = get_percentage_of_area(c)
-    
-    return np.concatenate((angles, ranges, hsv[0,0,1:], [similarity], [area]))
+    return np.concatenate((angles, ranges, hsv[0, 0, 1:], [similarity],
+                           [area]))
+
 
 def get_ranges_and_angles(points, x_c, y_c):
     """
+    Return ranges, their sum and angles from given points to center.
+
     Calculates and returns distances and angles of from given points to center
     and the sum of these distances.
-    
+
     Parameters
     ----------
     points : int32
@@ -141,67 +147,67 @@ def get_ranges_and_angles(points, x_c, y_c):
         X-coordinate of center of a contour.
     y_c : int
         Y-coordinate of center of a contour.
-        
+
     Returns
     -------
     tuple
-        Tuple containing distances and angles from points to the center of 
+        Tuple containing distances and angles from points to the center of
         contour. Each from them in float64.
-        
+
     """
     col = 0
     r_sum = 0
     ranges = np.zeros(len(points))
     angles = np.zeros(len(points))
-    
-    for i in range(0,len(points)):
-        dx = points[i,0,0] - x_c
-        dy = points[i,0,1] - y_c
-        
+
+    for i in range(0, len(points)):
+        dx = points[i, 0, 0] - x_c
+        dy = points[i, 0, 1] - y_c
+
         r, alpha = distance_and_angle(dx, dy)
         r_sum += r
 
         ranges[col] = r
         angles[col] = alpha
         col += 1
-        
     return (ranges, angles, r_sum)
+
 
 def ranges_in_percentage(ranges, r_sum):
     """
-    Calculates the percentage of each given range compared to their sum.
-    
+    Calculate the percentage of each given range compared to their sum.
+
     A symbol with different distances does not have the same range from each
     point to the center, but the percentage should not change. Therefore it is
     calculated here.
-    
+
     Parameters
     ----------
     ranges : float64
         Array of ranges from points of a contour to the center of this contour.
     r_sum : float64
         Sum of given ranges.
-        
+
     Returns
     -------
     Array
         Array containing all ranges in percentage of their sum in float64.
-        
+
     """
-    for col in range(0,len(ranges)):
+    for col in range(0, len(ranges)):
         ranges[col] = ranges[col] / r_sum
-    
+
     return ranges
+
 
 def data_between_nearest(points, x_c, y_c):
     """
-    Calculates and returns distance and angle from center to the middle of the 
-    two closest points and the coordinates of this middle.
-    
-    This function calculates the middle of the two closest significant points 
+    Return distance and angle from middle of nearest points to center.
+
+    This function calculates the middle of the two closest significant points
     in a contour and calculates distance and angle from center to the middle.
     Later, this middle can be used to sort the points.
-    
+
     Parameters
     ----------
     points : int32
@@ -210,33 +216,33 @@ def data_between_nearest(points, x_c, y_c):
         X-coordinate of center of a contour.
     y_c : int
         Y-coordinate of center of a contour.
-        
+
     Returns
     -------
     tuple
         Tuple containing distance and angle from the center of a contour to the
-        middle of the two closest points in a contour. Also the coordinates of 
+        middle of the two closest points in a contour. Also the coordinates of
         the middle of the closets points.
-        
+
     """
-    idx0,idx1 = find_nearest(points)
-    x_middle = int((points[idx1, 0, 0] + points[idx0, 0, 0]) /2)
-    y_middle = int((points[idx1, 0, 1] + points[idx0, 0, 1]) /2)
+    idx0, idx1 = find_nearest(points)
+    x_middle = int((points[idx1, 0, 0] + points[idx0, 0, 0]) / 2)
+    y_middle = int((points[idx1, 0, 1] + points[idx0, 0, 1]) / 2)
     dx = x_middle - x_c
     dy = y_middle - y_c
     dist, angle = distance_and_angle(dx, dy)
-    
     return (dist, angle, x_middle, y_middle)
+
 
 def sort_angles(angles, ranges, start_angle):
     """
     Sort angles and ranges starting from given angle.
-    
-    An angle and a distance describes a point. To find the same contour in 
+
+    An angle and a distance describes a point. To find the same contour in
     every rotation, an angle is used as reference. The same contour should have
     the same angles as long as the reference finds the rotation. The angles are
     all calculated positive and start from reference afterwards.
-    
+
     Parameters
     ----------
     angles : float64
@@ -245,65 +251,65 @@ def sort_angles(angles, ranges, start_angle):
         Array of ranges of points describing a contour.
     start_angle : float64
         Reference angle is used to sort ranges and angles in respect to it.
-        
+
     Returns
     -------
     tuple
         Tuple containing distances and angles now sorted after reference.
         All values are float64.
-        
+
     """
     for i in range(len(angles)):
         if (angles[i] < start_angle):
             angles[i] += 360
-        
+
         angles[i] -= start_angle
-            
+
     idx = angles.argsort()
     angles = angles[idx]
     ranges = ranges[idx]
-    
     return (angles, ranges)
+
 
 def get_color_in_hsv(im, c):
     """
     Get mean color inside a contour in hsv.
-    
+
     Parameters
     ----------
     im : Mat
         Image containing contours.
     c : int32
         Contour inside image.
-        
+
     Returns
     -------
     uint8
-        Matrix containing the mean values of a color in hsv space inside a 
+        Matrix containing the mean values of a color in hsv space inside a
         contour.
-        
+
     """
     mask = np.zeros(im.shape[:2], np.uint8)
     cv2.drawContours(mask, [c], 0, 255, -1)
     color = cv2.mean(im, mask)
-    hsv = cv2.cvtColor(np.uint8([[color[:3]]]),cv2.COLOR_BGR2HSV)
-    
+    hsv = cv2.cvtColor(np.uint8([[color[:3]]]), cv2.COLOR_BGR2HSV)
     return hsv
+
 
 def get_percentage_of_area(c):
     """
     Get percentage of contour area compared to the rectangle area.
-    
+
     Parameters
     ----------
     c : int32
         Contour used to calculate areas.
-        
+
     Returns
     -------
     float64
         Area of contour compared to its rectangular area.
-        
+
     """
     area = cv2.contourArea(c)
     rect = cv2.minAreaRect(c)
@@ -312,74 +318,75 @@ def get_percentage_of_area(c):
 
     return per_con_rect
 
+
 def distance_and_angle(dx, dy):
     """
-    Calculates and returns distance and angle of values.
-    
+    Calculate and returns distance and angle of values.
+
     Values are changed x- and y-values. Angle is calculated in degree,
     distance is calculated in pixel length, assumed pixelwidth is the same as
     pixelheight.
-    
+
     Parameters
     ----------
     dx : int64
         Change between the x-coordinates of two points.
     dy : int64
         Change between the y-coordinates of two points.
-        
+
     Returns
     -------
     tuple
         Tuple containing distance and angle from x and y changes of two points.
-        
+
     """
     r = np.sqrt(dy**2 + dx**2)
-    alpha = np.angle((dx + dy*1j), deg = True)
-    
+    alpha = np.angle((dx + dy*1j), deg=True)
+
     if (alpha < 0):
         alpha += 360
-        
+
     return (r, alpha)
-        
 
 
 def find_nearest(points):
     """
     Find the two points in a given list, which are closest to each other.
-    
+
     Returns index of values in this list. If length of list is equal or smaller
     than one, the two indices returned are 0.
-    
+
     Parameters
     ----------
     points : int32
         Points of a contour.
-   
+
     Returns
     -------
     tuple
         Tuple containing indices of the closest two points found, or two zeros
         for an error.
-        
+
     """
     if (len(points) <= 1):
         return (0, 0)
+
     else:
         dx = points[1, 0, 0] - points[0, 0, 0]
         dy = points[1, 0, 1] - points[0, 0, 1]
         dist_min, angle = distance_and_angle(dx, dy)
         idx0 = 0
         idx1 = 1
-        
+
         for i in range(len(points) - 1):
             for j in range(i + 1, len(points)):
                 dx = points[j, 0, 0] - points[i, 0, 0]
                 dy = points[j, 0, 1] - points[i, 0, 1]
                 dist, angle = distance_and_angle(dx, dy)
-                
+
                 if (dist < dist_min):
-                    dist_min = dist 
+                    dist_min = dist
                     idx0 = i
                     idx1 = j
-                    
+
         return (idx0, idx1)
--- test_picture.py
+++ test_picture.py
@@ -3,8 +3,8 @@
 """
 Published under GNU General Public License v3.0
 
-You should have recieved a copy of the license GNU GPLv3. 
-If not, see 
+You should have recieved a copy of the license GNU GPLv3.
+If not, see
 http://www.gnu.org/licenses/
 
 Search and measure from a picture.
@@ -31,7 +31,7 @@ MIN_PROBABILITY = 0.6
 #########################################################
 ap = argparse.ArgumentParser()
 ap.add_argument("-i", "--image", required=True,
-	help="input file for searching")
+                help="input file for searching")
 
 try:
     #######################################################
@@ -40,13 +40,13 @@ try:
     args = vars(ap.parse_args())
     filename = args["image"]
     im = cv2.imread(filename)
-    
+
     if im is None:
         raise FileNotFoundError("Image not found")
-        
+
     with open("arrow_ai.pkl", "rb") as f:
         ai = _pickle.load(f)
-    
+
     ######################################################################
     # Extract contours and search for the right contour.
     ######################################################################
@@ -54,32 +54,34 @@ try:
     con = ef.find_contours(im)
     data = np.zeros((1, ef.NUM_FEATURES))
     best_prob = -1
-    
+
     for i in range(len(con)):
         points, x_c, y_c = ef.get_shape_points(con[i])
-        
+
         if (len(points) == ef.NUM_POINTS):
             ranges, angles, r_sum = ef.get_ranges_and_angles(points, x_c, y_c)
             ranges = ef.ranges_in_percentage(ranges, r_sum)
             dist, alpha, x_m, y_m = ef.data_between_nearest(points, x_c, y_c)
             angles, ranges = ef.sort_angles(angles, ranges, alpha)
             hsv = ef.get_color_in_hsv(im, con[i])
-            similarity = np.float(cv2.matchShapes(con[i], shape_ref, 
+            similarity = np.float(cv2.matchShapes(con[i], shape_ref,
                                                   cv2.CONTOURS_MATCH_I2, 0))
             area = ef.get_percentage_of_area(con[i])
-            
-            data[0,:] = np.concatenate((angles, ranges, hsv[0,0,1:], [similarity], [area]))
+
+            data[0, :] = np.concatenate((angles, ranges, hsv[0, 0, 1:],
+                                        [similarity], [area]))
 
             # Use this line, if you just want to create a usable dataset.
-            # data[0,:] = ef.prepare_data(im, con[i], points, x_c, y_c, shape_ref)
-            
+            # data[0, :] = ef.prepare_data(im, con[i], points, x_c, y_c,
+            #                            shape_ref)
+
             prob = ai.predict_proba(data)
             proba = prob[0][1]
-        
+
             if (proba >= MIN_PROBABILITY):
                 if (proba > best_prob):
                     d = cam_data[0] * cam_data[1] / (dist * 10)
-    
+
                     best_x_c = x_c
                     best_y_c = y_c
                     best_x_m = x_m
@@ -104,46 +106,50 @@ try:
         print("AI precision: ", best_prob)
         print("Angles: ", best_angles)
         print("Ranges: ", best_ranges)
-        print("Color: ", best_hsv[0,0,1:])
+        print("Color: ", best_hsv[0, 0, 1:])
         print("Difference to reference: ", best_sim)
-        print("Contour area / rectangular area ", best_area )
+        print("Contour area / rectangular area ", best_area)
         print("Distance to arrow: ", best_d)
         print("Angle of arrow: ", 180 + best_alpha)
         dx_px = x_c - im.shape[1]/2
         dy_px = y_c - im.shape[0]/2
-        print("The horizontal distance from center of arrow to center of image is ", dx_px,
-              " pixel and the vertical distance is ", dy_px, " pixel.")
-                
+        print("The horizontal distance from center of arrow to center of "
+              + "image is ", dx_px, " pixel and the vertical distance is ",
+              dy_px, " pixel.")
+
         for j in range(len(best_points)):
-            cv2.line(im, (best_points[j, 0, 0], best_points[j, 0, 1]), 
-                     (best_points[j, 0, 0], best_points[j, 0, 1]), (0, 0, 255), 
+            cv2.line(im, (best_points[j, 0, 0], best_points[j, 0, 1]),
+                     (best_points[j, 0, 0], best_points[j, 0, 1]), (0, 0, 255),
                      10)
-        
+
         cv2.drawContours(im, con, best_i, (255, 0, 0), 2)
-        cv2.line(im,(best_x_c, best_y_c), (best_x_c, best_y_c), (255, 255, 0), 20)
-        cv2.line(im,(best_x_m, best_y_m), (best_x_m, best_y_m), (0, 255, 0), 15)
-        cv2.putText(im, "{:.0f}".format(best_d) + " cm", 
-                    (10,  im.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.5, 
-                    (255, 0, 0), 3)    
-        cv2.putText(im, "{:.2f}".format(best_prob) + " probability", 
-                    (230,  im.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.5,
-                    (255, 0, 0), 3)    
+        cv2.line(im, (best_x_c, best_y_c), (best_x_c, best_y_c), (255, 255, 0),
+                 20)
+        cv2.line(im, (best_x_m, best_y_m), (best_x_m, best_y_m), (0, 255, 0),
+                 15)
+        cv2.putText(im, "{:.0f}".format(best_d) + " cm", (10,
+                    im.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.5,
+                    (255, 0, 0), 3)
+        cv2.putText(im, "{:.2f}".format(best_prob) + " probability", (230,
+                    im.shape[0] - 10), cv2.FONT_HERSHEY_SIMPLEX, 1.5,
+                    (255, 0, 0), 3)
 
     else:
         print("No arrow found.")
-        
+
     cv2.namedWindow(filename, cv2.WINDOW_NORMAL)
     cv2.resizeWindow(filename, 640, 480)
-    cv2.imshow(filename,im)
-    
+    cv2.imshow(filename, im)
+
     # Just close the window or press any key.
     while True:
         if cv2.getWindowProperty(filename, cv2.WND_PROP_VISIBLE):
-            if cv2.waitKey(100) != -1: # Any key pressed.
+            if cv2.waitKey(100) != -1:  # Any key pressed.
                 break
+
         else:
             break
-    
+
     cv2.destroyAllWindows()
     print("done")
 
@@ -153,8 +159,4 @@ except FileNotFoundError as err:
 
 # Option to abort.
 except KeyboardInterrupt:
-    print("Aborted")    
-    
-# Default.
-except:
-    print("Something went wrong.")
+    print("Aborted")
