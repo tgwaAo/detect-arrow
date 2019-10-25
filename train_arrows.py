c1f523bdb5900665a849fdfaa8d8020af771635f
tgwaAo <toolongtext@gmail.com>
2019-10-25 15:24:37 +0200

    Used pycodestyle and corrected errors.

--- train_arrows.py
+++ train_arrows.py
@@ -3,8 +3,8 @@
 """
 Published under GNU General Public License v3.0
 
-You should have recieved a copy of the license GNU GPLv3. 
-If not, see 
+You should have recieved a copy of the license GNU GPLv3.
+If not, see
 http://www.gnu.org/licenses/
 
 Train ai with features.
@@ -34,10 +34,12 @@ print("Number of non arrows ", len(target) - num_one)
 #############################################################
 # Split data for training and testing, start training.
 #############################################################
-x_train, x_test, y_train, y_test = train_test_split(features, target, test_size=0.2)
+x_train, x_test, y_train, y_test = train_test_split(features, target,
+                                                    test_size=0.2)
 
 kernel = 1.0 * RBF(1.0)
-gpc = GaussianProcessClassifier(kernel=kernel, random_state=0).fit(x_train, y_train)
+gpc = GaussianProcessClassifier(kernel=kernel, random_state=0).fit(x_train,
+                                                                   y_train)
 
 ##############################################################
 # Testing and saving.
