9ad663a4a8284e3cda13223dc712352e318af5a1
tgwaAo <toolongtext@gmail.com>
2025-03-11 00:37:32 +0100

    prepared pose estimation in stream

---
+++ io/video.py
@@ -0,0 +1,44 @@
+#!/bin/env python3
+
+from queue import Queue
+
+import cv2
+
+class VideoCapture:
+    def __init__(self, *args, **kwargs):
+        self.image = None
+        self.stopped = False
+        self.Q = Queue(maxsize=2)
+        self.cap = cv2.VideoCapture(*args, **kwargs)
+        self.lock = threading.Lock()
+        self.event = threading.Event()
+        self.t = threading.Thread(target=self._reader)
+        self.t.start()
+
+    def _reader(self):
+        while not self.event.is_set():
+            with self.lock:
+                if self.Q.full():
+                    _ = self.Q.get()  # remove value for new ones
+
+                ret, image = self.cap.read()
+                if not ret:
+                    self.cap.release()
+                    raise ValueError('could not get image from VideoCapture')
+
+                self.Q.put(image)
+
+    def read(self):
+        return True, self.Q.get()
+
+    def isOpened(self):
+        return self.cap.isOpened()
+
+    def set(self, *args):
+        with self.lock:
+            self.cap.set(*args)
+
+    def release(self):
+        self.event.set()
+        self.t.join()
+        self.cap.release()
