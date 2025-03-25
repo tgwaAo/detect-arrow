ae15849548340523db3530ecffe0e31b489c8520
tgwaAo <toolongtext@gmail.com>
2025-03-25 21:59:18 +0100

    made camera calibration runable

---
+++ main/03_calibrate_camera.py
@@ -0,0 +1,36 @@
+#!/bin/env python3
+
+from glob import glob
+from pathlib import PurePath
+
+import cv2
+import numpy as np
+
+from main.conf.paths import CALIB_IMGS_PATH
+from main.processing.calibrate import Calibrator
+
+if __name__ == '__main__':
+    calib = Calibrator()
+    calib.read_printed_nbrs()
+    if not calib.img_corners_into_list():
+        exit(1)
+    calib.prepare_undistortion()
+    img_fname = glob(str(PurePath(CALIB_IMGS_PATH, '*.jpg')))[0]
+    img = cv2.imread(img_fname)
+    if img is None:
+        exit(1)
+    result = calib.undistort(img)
+    combined = np.hstack((img, result))
+    cv2.imshow('calibration', combined)
+
+    merged = cv2.addWeighted(img, 0.5, result, 0.5, 0)
+    cv2.imshow('merged', merged)
+    cv2.waitKey(0)
+
+    cv2.destroyAllWindows()
+
+    result = calib.error()
+    if result is not None:
+        print(f'total error: {result}')
+    else:
+        print('error could not be calculated')
